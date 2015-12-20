f07df52f2e2004499be9180884cc4af14fd13c84
aysebasakkoca <aysebasakkoca@gmail.com>
2015-12-20 21:09:52 +0200

    Created Project

---
+++ DamacanaStore/DamacanaStore/Areas/HelpPage/ModelDescriptions/EnumTypeModelDescription.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace DamacanaStore.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumTypeModelDescription : ModelDescription
+    {
+        public EnumTypeModelDescription()
+        {
+            Values = new Collection<EnumValueDescription>();
+        }
+
+        public Collection<EnumValueDescription> Values { get; private set; }
+    }
+}
